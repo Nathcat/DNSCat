5feaaee9154f554f5b5e0e4766738eb4c5e148cb
Nathcat <74734054+Nathcat@users.noreply.github.com>
2025-08-08 14:07:45 +0100

    Testing message parsing

--- app/src/main/java/net/nathcat/dnscat/Main.java
+++ app/src/main/java/net/nathcat/dnscat/Main.java
@@ -1,8 +1,11 @@
 package net.nathcat.dnscat;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+
+import net.nathcat.dnscat.Message.Message;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -19,8 +22,12 @@ public class Main {
         while (true) {
             DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
             socket.receive(packet);
-            
+
+            byte[] data = new byte[packet.getLength()];
+            System.arraycopy(packet.getData(), packet.getOffset(), data, 0, packet.getLength());
+            Message msg = new Message(new ByteArrayInputStream(data));
             System.out.println("Got datagram from " + packet.getAddress().getHostAddress() + " on port " + packet.getPort());
+            System.out.println(msg);
         }
     }
 }
